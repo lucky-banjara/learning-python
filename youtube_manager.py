e5efd36e2fbdc67e8c26beb7cbd4b8b3a36b2620
Anjaan Banjara <luckybanjara.anjaan@gmail.com>
2026-06-02 00:12:08 +0800

    List, Add, Update, Delete Function Implemented

--- youtube_manager.py
+++ youtube_manager.py
@@ -7,18 +7,45 @@ def load_data():
               return json.load(file)
      except FileNotFoundError:
           return []
+     
+def save_data_helper(videos):
+     with open('youtube.txt', 'w') as file:
+          json.dump(videos, file)
 
 def list_all_videos(videos):
-    pass
-
+    print("\n")
+    print("*"*70)
+    for index, video in enumerate(videos, start=1):
+         print(f"{index}. Video Name:{video['name']}, Duration: {video['time']}  ")
+    print("\n")
+    print("*"*70) 
+    
 def add_video(videos):
-    pass
+   name= input("Enter video name:")
+   time= input("Enter video time:")
+   videos.append({'name':name, 'time':time})
+   save_data_helper(videos)
 
 def update_video(videos):
-    pass
+    list_all_videos(videos)
+    index =int(input("Enter the number of the video you want to update:"))
+    if 1 <= index <= len(videos):
+        name = input("Enter the new video name:")
+        time = input("Enter the new video time:")
+        videos[index-1] = {'name':name, 'time':time}
+        save_data_helper(videos)
+    else:
+        print("Invalid index selected. Please try again.")
 
 def delete_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the number of the video you want to delete:"))
+    if 1 <= index <= len(videos):
+         del videos[index-1]
+         save_data_helper(videos)
+    else:
+         print("Invalid index selected. Please try again.")
+
 
 def main():
         videos = load_data()
@@ -30,7 +57,8 @@ def main():
                 print("3. Update a youtube video details")
                 print("4. Delete a youtube video")
                 print("5. Exit the Application")
-                choice = input("Enter your choice:")
+                choice = input("Enter your choice: ")
+                # print (videos)
 
                 match choice:
                     case '1':
